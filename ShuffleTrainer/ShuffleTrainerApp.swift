569c4758de6ea4377c1b4ce65f41523f253955eb
Adam Nguyen <Adam0lifez@gmail.com>
2023-07-24 14:42:59 -0400

    Initial Commit

---
+++ ShuffleTrainer/ShuffleTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuffleTrainerApp.swift
+//  ShuffleTrainer
+//
+//  Created by Adam Nguyen on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ShuffleTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
